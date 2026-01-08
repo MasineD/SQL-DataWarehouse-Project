aca30c9622ec54cc269837eecd2dda6d1d5da39e
MasineD <masinedonald@gmail.com>
2026-01-08 16:58:29 +0200

    This creates the VIEWS in the Gold layer, used to access data from the Silver layer

--- scripts/GoldLayer/CreateViews_Gold.sql
+++ scripts/GoldLayer/CreateViews_Gold.sql
@@ -1,5 +1,15 @@
---USE DataWarehouseDB
+/*
+	PURPOSE
+		1.This creates the VIEWS used to access data in the Gold layer, which presents the final dimension
+		  and fact tables using a Star Schema
+		2.Each view performs transformations and combines data from different tables in the Silver layer to
+		  produce cleaned,enriched, and business-ready datasets
 
+	USAGE:
+		1.The datasets can be used for analytics and reporting
+*/
+
+/************ Creating view for the CustomerInfo *************/
 --NOTE: The CRM is the master for gender details
 CREATE VIEW Gold.dim_CustomerInfo AS
 SELECT ROW_NUMBER() OVER(ORDER BY ci.CustomerID) AS CustomerKey, ci.CustomerID, ci.CustomerKey AS CustomerNumber, ci.CustomerFirstName FirstName
@@ -15,5 +25,23 @@ LEFT JOIN Silver.erp_Location l
 ON ci.CustomerKey = l.CustomerID
 WHERE ci.CustomerID IS NOT NULL;
 
+/************ Creating view for the Products information *************/
+CREATE VIEW Gold.dim_Products AS
+SELECT ROW_NUMBER() OVER(ORDER BY  p.ProductStartDate,p.ProductKey) AS SurrKey, p.ProductID, p.ProductKey, p.ProductName, p.CategoryID,pc.Category, pc.SubCategory, pc.Maintenance
+	, p.ProductCost, p.ProductLine, p.ProductStartDate, p.ProductEndDate
+FROM Silver.crm_ProductInfo p
+LEFT JOIN Silver.erp_ProductsCatalogue pc
+ON p.CategoryID = pc.ID
+WHERE ProductEndDate IS NULL;		--Filtering the historical data
+
+/************ Creating view for the Sales information *************/
+CREATE VIEW Gold.fact_Sales AS
+SELECT OrderNumber, gp.SurrKey AS ProductKey, gc.CustomerKey, OrderDate, ShipDate, DueDate, Sales, Quantity, Price
+FROM Silver.crm_SalesDetails s
+LEFT JOIN Gold.dim_Products gp
+ON s.ProductKey = gp.ProductKey
+LEFT JOIN Gold.dim_CustomerInfo gc
+ON s.CustomerID = gc.CustomerID
+
 SELECT *
-FROM Gold.dim_CustomerInfo
+FROM Gold.fact_Sales
